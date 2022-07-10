b2be35436de6cc6817289ce36b701e304875edd7
chandan chakma <cchakma19@gmail.com>
2022-07-09 21:04:22 -0700

    add inventory style

--- src/pages/Store/Inventories/Inventories.js
+++ src/pages/Store/Inventories/Inventories.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import UseInventories from '../../../Hook/UseInventories';
 import Inventory from '../Inventory/Inventory';
+import "./inventories.css"
 
 const Inventories = () => {
     const [inventories, setInventories] = UseInventories();
 
     return (
         <div className='m-4'>
-            <h1 className='text-center'>Our Inventory</h1>
-            {
-                inventories.map(inventory => <Inventory
-                    key={inventory._id}
-                    inventory={inventory}
 
-                >
+            <h1 className='text-center m-5'>Our Inventory</h1>
 
-                </Inventory>)
-            }
+            <div className='inventories'>
+                {
+                    inventories.map(inventory => <Inventory
+                        key={inventory._id}
+                        inventory={inventory}
+
+                    >
+
+                    </Inventory>)
+                }
+
+            </div>
+
 
         </div>
     );
--- src/pages/Store/Inventory/Inventory.js
+++ src/pages/Store/Inventory/Inventory.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './inventory.css';
 
 const Inventory = ({ inventory }) => {
+    const { name, price, description, img, supplier } = inventory;
     return (
-        <div>
-            <h2>{inventory.name}</h2>
+        <div className='inventory'>
+            <img src={img} alt="" srcset="" />
+            <h2>{name}</h2>
+            <h2>{description}</h2>
+            <p>Price: {price}</p>
+            <h6>{supplier}</h6>
+            <button>update</button>
 
         </div>
     );
